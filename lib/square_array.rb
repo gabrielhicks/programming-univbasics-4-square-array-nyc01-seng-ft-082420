e8d3400c8812a9a7ae40687ffc17571a47a92650
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-05 19:57:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   length = array.length
   new_array = []
   
-  while new_array.length < length do |index|
+  while new_array.length < length do
     new_array.push(array[index] ** 2)
   end
   
